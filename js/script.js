befb47e9f048a1b8b83f60e612b9baba2a98eec2
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-09-16 23:23:35 +0000

    initial setup done

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let serverURL;
+let serverPort;
+let url;
+
+$.ajax({
+  url: 'config.json',
+  type: 'GET',
+  dataType: 'json',
+  success:function(keys){
+    serverURL = keys['SERVER_URL'];
+    serverPort = keys['SERVER_PORT'];
+    url = `${keys['SERVER_URL']}:${keys['SERVER_PORT']}`;
+  },
+  error: function(){
+    console.log('cannot find config.json file, cannot run application');
+  }
+});
