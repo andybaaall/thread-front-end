52c3a29b223bfc337b43111ea6d421452cb577fd
Sophie Kim <270010565@bun2-206-25.acg.ac.nz>
2019-09-19 08:33:56 +1200

    clear

--- js/script.js
+++ js/script.js
@@ -20,3 +20,44 @@ $('#loginBtn').click(function(){
   $('.main').addClass('d-none');
   $('#loginForm').removeClass('d-none');
 })
+//
+// $('#loginForm').submit(function(){
+//   event.preventDefault();
+//   const username = $('#lUsername').val();
+//   const password = $('#lPassword').val();
+//
+//   if ((username.length === 0)||(password.length === 0)) {
+//       console.log('Please enter your username and password');
+//   } else {
+//     $.ajax({
+//       url: `${url}/getUser`,
+//       type: 'GET',
+//       data: {
+//         username: username,
+//         password: password
+//       },
+//       success: function(result){
+//           if (result === 'invalid user') {
+//             console.log('cannot find user with that username');
+//           } else if (result === 'invalid password') {
+//             console.log('your password id wrong');
+//
+//           } else {
+//             console.log('lets log you in');
+//
+//             $('#authForm').modal('hide');
+//             $('#loginBtn').text('log out');
+//
+//             $('#jumbotron-heading').text('Welcome ' + username)
+//
+//             $('.btn-link').removeClass('d-none');
+//
+//           }
+//       },
+//       error: function(err){
+//           console.log(err);
+//           console.log('Something went wrong');
+//       }
+//     })
+//   }
+// });
