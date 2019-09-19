10949a8f259f002fa1b6a67a4acc74b7138e7b46
Andy Ball <932400399@bun2-206-14.local>
2019-09-20 11:29:54 +1200

    added csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,137 +24,30 @@ module.exports = function(grunt){
             src: ['css/style.css']
           }
       },
+      cssmin: {
+          target: {
+              files: [{
+                  expand: true,
+                  src: ['css/*.css', 'css/!*.min.css'],
+                  dest: '',
+                  ext: '.min.css'
+              }]
+          }
+      },
         watch: {
             files: ['<%= jshint.files %>', 'css/style.css'],
-            tasks: ['jshint', 'csslint']
+            tasks: ['jshint', 'csslint', 'cssmin']
         }
     });
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // register tasks
     grunt.registerTask('checkJS', ['jshint']);
     grunt.registerTask('checkCSS',['csslint']);
+    grunt.registerTask('minifyCSS', ['cssmin']);
     grunt.registerTask('runWatch', ['watch']);
 };
-
-
-
-// module.exports = function(grunt){
-//
-//   // CONFIGS -------------------------------------------------------------------
-//   grunt.initConfig({
-//     csslint: {
-//       strict: {
-//         options: {
-//           import: 2
-//         },
-//         src: ['css/*.css']
-//       },
-//       lax: {
-//         options: {
-//           import: false
-//         },
-//         src: ['css/*.css']
-//       }
-//     },
-//     // Sophie working above section
-//
-//     jshint: {
-//       files: ['Gruntfile.js', 'js/script.js'],
-//       options: {
-//         globals: {
-//           jQuery: true
-//         },
-//         curly: true,
-//         eqeqeq: true,
-//         esversion: 6
-//       }
-//     },
-//     // Andy working above section
-//
-//     watch: {
-//       css: {
-//         files: ['css/*.css'],
-//         tasks: ['csslint']
-//       },
-//       js: {
-//         files: ['js/*.js', '!js/*.min.js'],
-//         tasks: ['jshint']
-//       }
-//     },
-//     //Matt working above section
-//
-//     // John working above this section
-//   });
-// // CONFIGS ^^^^^^  -------------------------------------------------------------
-//
-// // LOAD TASKS ------------------------------------------------------------------
-//   grunt.initConfig({
-//     // csslint: {
-//     //   strict: {
-//     //     options: {
-//     //       import: 2
-//     //     },
-//     //     src: ['css/*.css']
-//     //   },
-//     //   lax: {
-//     //     options: {
-//     //       import: false
-//     //     },
-//     //     src: ['css/*.css']
-//     //   }
-//     // },
-//     // Sophie working above section
-//
-//
-//     jshint: {
-//         files: ['*.js'],
-//         options: {
-//           globals: {
-//             jQuery: true
-//           },
-//           esversion: 6
-//         }
-//       }
-// //Andy working above section
-//   //   watch: {
-//   //     css: {
-//   //       files: ['css/*.css'],
-//   //       tasks: ['csslint']
-//   //     },
-//   //     js: {
-//   //       files: ['*.js, js/*.js'],
-//   //       tasks: ['jshint']
-//   //     }
-//   //   }
-//   // });
-// //Matt working above section
-//
-// // grunt.loadNpmTasks('grunt-contrib-csslint');
-// // Sophie working above section
-//
-// grunt.loadNpmTasks('grunt-contrib-jshint');
-// //Andy working above section
-//
-// // grunt.loadNpmTasks('grunt-contrib-watch');
-// //Matt working above section
-//
-// // LOAD TASKS ^^^^^^  ----------------------------------------------------------
-//
-// // REGISTER TASKS ^^^^^^  ------------------------------------------------------
-//
-// // grunt.registerTask('csslint',['csslint']);
-// // Sophie working above section
-//
-// grunt.registerTask('jshint',['jshint']);
-// //Andy working above section
-//
-// // grunt.registerTask('runWatch', ['watch']);
-// //Matt working above section
-//
-// // REGISTER TASKS  ^^^^^^  ------------------------------------------------------
-//
-// };
