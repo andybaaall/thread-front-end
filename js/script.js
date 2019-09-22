dad73af9b038707276f6ce1b383c1ebb5b6397c9
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-09-22 20:54:41 +1200

    can't find anything

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ $(document).ready(function(){
       // you're logged in, nice :)
       $('#logInOutBox').append(logoutBtn);
       logout();
+      getItemsData();
   } else {
       // you're not logged in
       $('#logInOutBox').append(loginBtn);
@@ -38,15 +39,13 @@ $.ajax({
     serverURL = keys['SERVER_URL'];
     serverPort = keys['SERVER_PORT'];
     url = `${keys['SERVER_URL']}:${keys['SERVER_PORT']}`;
-    getItemsData();
-    // need to run a function to get all the items data, right?
     // getItemsData();
+    // need to run a function to get all the items data, right?
   },
   error: function(){
     console.log('cannot find config.json file, cannot run application');
   }
 });
-
 
 getItemsData = () => {
   $.ajax({
@@ -64,7 +63,47 @@ getItemsData = () => {
 }
 
 
+$('#uploadBtn').click(function(){
+  event.preventDefault();
+  let itemName = $('#itemName').val();
+  let clothingType = $('#clothingType').val();
+  let price = $('#price').val();
+  let condition = $('#condition').val();
+  console.log(`${itemName} - $${price}`);
+  $.ajax({
+    url: `${url}:${serverPort}/item`,
+    type: 'post',
+    data: {
+      itemName: item_name,
+      price: price,
+      condition: condition,
+      success: function(result){
+        $('.itemList').append(`<h5 class="card-title text-center mt-3" data-id="${result[i].itemName}">${result[i].price}</h5>`)
+      },
+      error: function(err){
+        console.log(err);
+        console.log('Something went wrong');
+      }
+    }
+  })
+  $('#cardContainer').removeClass('d-none');
+  $('.uploadModal').remove();
+  $('.modal-backdrop').remove();
+})
 
+
+// <div class="card col-6">
+// <img id="workImg" src="${data[i].imgURL}" class="card-img-top">
+//  <div>
+//  <div id="worktitle" class="card-title">
+//  <h5 class="card-title text-center mt-3" data-id="${data[i].itemName}">${data[i].itemName}</h5>
+//   <p class="text-center">${data[i].price}</p>
+//   </div>
+//   <div class="d-flex justify-content-between align-items-center btn-group">
+//   <button class="btn btn-primary" type="button" name="button">Detail</button>
+//   </div>
+//   </div>
+//   </div>
 
 $('.registerSubmit').click(function(){
   event.preventDefault();
@@ -139,8 +178,8 @@ $('.loginSubmit').click(function(){
                  $('#userForm').addClass('d-none');
                  $('.main').removeClass('d-none');
                  $('#cardContainer').removeClass('d-none');
+                   getItemsData();
                  ////////
-
                  // result.username -> sessionStorage.username
                  // result.user_id -> sessionStorge.user_id
                  // this bad baby tells us who's logged in
@@ -154,9 +193,3 @@ $('.loginSubmit').click(function(){
     })
   }
 });
-
-$('#uploadBtn').click(function(){
-  $('#cardContainer').removeClass('d-none');
-  $('.uploadModal').remove();
-  $('.modal-backdrop').remove();
-})
