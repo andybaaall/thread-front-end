c2082bc158ef8ad2141554f0170532f68d143be9
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-09-24 23:11:00 +1200

    more work

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ let serverPort;
 let url;
 let editing = false;
 $(document).ready(() => {
-
     // get the config.json
     $.ajax({
         url: 'config.json',
@@ -13,15 +12,14 @@ $(document).ready(() => {
             serverURL = keys.SERVER_URL;
             serverPort = keys.SERVER_PORT;
             url = `${keys.SERVER_URL}:${keys.SERVER_PORT}`;
+              showItems();
         },
         error: function(){
             console.log('cannot find config.json file, cannot run application');
         }
     });
 
-    showItems();
-
-    if (sessionStorage.username) {
+    if (sessionStorage.userName) {
         // user is logged in
         showLogoutBtn();
     } else {
@@ -32,41 +30,41 @@ $(document).ready(() => {
 });
 
 showItems = () => {
-        $.ajax({
-            url: `${url}/allItems`,
-            type: 'GET',
-            dataType: 'json',
-            success: function(data){
-                console.log(data);
-                $('#cardContainer').find('.row').empty();
-                for (var i = 0; i < data.length; i++) {
-                    let layout = `
-                        <div class="col-12 col-md-3">
-                            <div class="card" data-id="${data[i]._id}">
-                                <div class="card-body">
-                                    <div id="worktitle" class="card-title">
-                                        <h5 class="card-title text-center mt-3" >${data[i].item_name}</h5>
-                                        <p class="text-center">${data[i].price}</p>
+    $.ajax({
+        url: `${url}/allItems`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            console.log(data);
+            $('#cardContainer').find('.row').empty();
+            for (var i = 0; i < data.length; i++) {
+                let layout = `
+                    <div class="col-12 col-md-3">
+                        <div class="card" data-id="${data[i]._id}">
+                            <div class="card-body">
+                                <div id="worktitle" class="card-title">
+                                    <h5 class="card-title text-center mt-3" >${data[i].item_name}</h5>
+                                    <p class="text-center">${data[i].price}</p>
+                                </div>`;
+                                if (sessionStorage.userName) {
+                                    layout += `<div class="btnSet d-flex justify-content-center">
+                                    <button class="btn btn-primary btn-sm mr-1 editBtn">EDIT</button>
+                                    <button class="btn btn-secondary btn-sm removeBtn">REMOVE</button>
                                     </div>`;
-                                    if (sessionStorage.userName) {
-                                        layout += `<div class="btnSet d-flex justify-content-center">
-                                        <button class="btn btn-primary btn-sm mr-1 editBtn">EDIT</button>
-                                        <button class="btn btn-secondary btn-sm removeBtn">REMOVE</button>
-                                        </div>`;
-                                    }
-                                layout += `</div>
-                            </div>
+                                }
+                            layout += `</div>
                         </div>
-                    `;
-                    $('#cardContainer').find('.row').append(layout);
-                }
-                $('#cardContainer').removeClass('d-none');
-            },
-            error: function(err){
-                console.log(err);
-                console.log('Something went wrong');
+                    </div>
+                `;
+                $('#cardContainer').find('.row').append(layout);
             }
-        });
+            $('#cardContainer').removeClass('d-none');
+        },
+        error: function(err){
+            console.log(err);
+            console.log('Something went wrong');
+        }
+    });
 };
 
 
@@ -103,22 +101,49 @@ const clearForms = () => {
 
 // these all show DOM elements
 const showLoginBtn = () => {
-
+  $('#logInOutBox').append(`<button id="loginBtn" class="btn btn-light" type="button" name="button" onclick="login()">Login</button>`);
+  login = () => {
+    console.log('clicked login button');
+        $('.main').addClass('d-none');
+        $('#userForm').removeClass('d-none');
+        $('#loginFormBox').removeClass('d-none');
+        $('#lUsername').val(null);
+        $('#lPassword').val(null);
+  };
 };
 const showLogoutBtn = () => {
-
+  $('#logInOutBox').append(`<button id="logoutBtn" class="btn btn-light" type="button" name="button" onclick="logout()">Logout</button>`);
+  logout = () => {
+    console.log('clicked logout button');
+      $('#logInOutBox').empty();
+      showRegisterBtn();
+      showLoginBtn();
+      sessionStorage.clear();
+      $('#cardContainer').addClass('d-none');
+  };
 };
 const showRegisterBtn = () => {
-
+  $('#logInOutBox').append(`<button id="registerBtn" class="mr-1 btn btn-light" type="button" name="button" onclick="register()">Register</button>`);
+  register = () => {
+    console.log('clicked register button');
+        $('.main').addClass('d-none');
+        $('#userForm').removeClass('d-none');
+        $('#loginFormBox').addClass('d-none');
+        $('#registerFormBox').removeClass('d-none');
+        $('#rUsername').val(null);
+        $('#rEmail').val(null);
+        $('#rPassword').val(null);
+        $('#rConfirmPassword').val(null);
+  };
 };
-const showLoginForm = () => {
-
-};
-const showRegisterForm = () => {
-
-};
+// const showLoginForm = () => {
+//
+// };
+// const showRegisterForm = () => {
+//
+// };
 const showAddItemForm = () => {
-
+  $('#uploadModal').show();
 };
 const showEditItemForm = () => {
 
@@ -153,6 +178,53 @@ $('#loginBtn').click(() => {
 });
 
 $('#loginForm').submit(() => {
+  event.preventDefault();
+
+ const username = $('#lUsername').val();
+ const password = $('#lPassword').val();
+
+ if ((username.length === 0)||(password.length === 0)) {
+     console.log('Please enter your username and password');
+ } else {
+     $.ajax({
+         url: `${url}/getUser`,
+         type: 'POST',
+         data: {
+             username: username,
+             password: password
+         },
+         success: function(result){
+             if (result === 'user does not exist'){
+                 console.log('user does not exist');
+             } else if (result === 'invalid password'){
+                 console.log('invalid password');
+             } else {
+                 sessionStorage.setItem('userID', result._id);
+                 sessionStorage.setItem('userName', result.username);
+                 sessionStorage.setItem('userEmail', result.email);
+
+                 $('#logInOutBox').empty();
+                 showLogoutBtn();
+
+                 $('#userForm').addClass('d-none');
+                 $('.main').removeClass('d-none');
+                 $('#addListBtn').removeClass('d-none');
+
+                 const addItemBox = `<div class="addItemBox mx-5 my-2 text-left"><form id="addItemForm" enctype="multipart/form-data"><div class="form-group"><label for="itemName">Item Name</label><input type="text" class="form-control" id="itemName"></div><div class="form-group"><label for="itemDescription">Description</label><input type="text" class="form-control" id="itemDescription"></div><div class="form-group"><label for="itemPrice">Price</label><input type="number" class="form-control" id="itemPrice"></div><div class="input-group"><div class="input-group-prepend"><span class="input-group-text" id="itemImagePrepend">Item image</span></div><div class="custom-file"><input type="file" class="custom-file-input" id="itemImage"><label class="custom-file-label" for="itemImage">Choose file</label></div></div><fieldset class="form-group"><div class="row"><legend class="col-form-label col-sm-2 pt-0">Clothing Type</legend><div class="col-sm-10"><div class="form-check"><input class="form-check-input" type="radio" id="topsRadio" value="Tops" name="itemType" ><label class="form-check-label" for="topsRadio">Tops</label></div><div class="form-check"><input class="form-check-input" type="radio" id="bottomsRadio" value="Bottoms" name="itemType"><label class="form-check-label" for="bottomsRadio">Bottoms</label></div><div class="form-check"><input class="form-check-input" type="radio" id="outwewearRadio" value="Outerwear" name="itemType"><label class="form-check-label" for="outwewearRadio">Outerwear</label></div><div class="form-check"><input class="form-check-input" type="radio" id="otherRadio" value="Other" name="itemType"><label class="form-check-label" for="otherRadio">Other</label></div></div></div></fieldset><fieldset class="form-group"><div class="row"><legend class="col-form-label col-sm-2 pt-0">Condition</legend><div class="col-sm-10"><div class="form-check"><input class="form-check-input" type="radio" id="newRadio" value="New" name="itemCondition"><label class="form-check-label" for="newRadio">New</label></div><div class="form-check"><input class="form-check-input" type="radio" id="usedRadio" value="Used" name="itemCondition"><label class="form-check-label" for="usedRadio">Used</label></div></div></div></fieldset><button id= "addItemSubmitBtn" type="submit" class="btn btn-primary">add an item</button></form></div>`;
+
+                 $('#pageContainer').append(addItemBox);
+                 showItems(); // we need to rework this so that it appends and empties, rather than switches from d-flex to d-none
+                 // addItem();
+             }
+         },
+         error: function(err){
+             console.log(err);
+             console.log('Something went wrong');
+         }
+     });
+ }
+
+
     if (sessionStorage.username) {
         // user is logged in. This bad joke needs a lot of explanation.
         console.log('PERMISSION DENIED');
